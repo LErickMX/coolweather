24fe6c8e092c6d60075c7f24d0fb97c801e11be2
sting <957109631@qq.com>
2018-11-10 00:01:46 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,46 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by hydn on 2018/11/9.
+ */
+
+public class City extends DataSupport {
+    private int id;            //
+    private String cityName;  //城市名字
+    private int cityCode;    //城市代号
+    private int provinceId;  //城市所属省的id值
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by hydn on 2018/11/9.
+ */
+
+public class Province extends DataSupport {
+    private int id;              //每个实体类应有的
+    private String provinceName; //省名字
+    private  int provinceCode;   //省的代号
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+}
